f82c1ee13f742e9201fb01e1cc03a92840541024
AHachey17 <ahachey17@gmail.com>
2023-03-27 09:57:57 -0400

    want to be more consistent

---
+++ Day8IncorrectDivision.js
@@ -0,0 +1,10 @@
+//This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+
+/*Given: 
+const solve = (x, y) => x / y
+*/
+
+//Solution:
+
+const solve = (x, y) => x / y
+
